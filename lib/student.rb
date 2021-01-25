af4cf3b37d190c4ef4227079a688138542c925b5
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-25 01:14:18 -0500

    initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,56 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      CREATE TABLE IF EXISTS students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES ( ?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
+    end
+  end
+
+  def self.new_from_db(student)
+    @id = student[0]
+    @name = student[1]
+    @grade = student[2]
+  end
+
+  def self.find_by_name
+  end
+
+  def update
+  end
 
 
 end
